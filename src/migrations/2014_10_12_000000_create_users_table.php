9d639beb2f20ebcfd5e760a5203014b8adf4c9c6
Martin Schäpker <martin.schaepker@tredex.de>
2015-03-18 12:09:46 +0100

    change table structur

--- src/migrations/2014_10_12_000000_create_users_table.php
+++ src/migrations/2014_10_12_000000_create_users_table.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-class CreateUsersTable extends Migration {
+
+/**
+ * Migration class to create the users table
+ *
+ */
+class CreateUsersTable extends Migration
+{
+
+    // Tabellennamen festlegen
+    const TABLENAME = 'users';
+
     /**
      * Run the migrations.
      *
@@ -10,16 +20,35 @@ class CreateUsersTable extends Migration {
      */
     public function up()
     {
-        Schema::create('users', function(Blueprint $table)
+        Eloquent::unguard();
+        $actDB = ( Config::get( 'database.default' ) === 'mysql' ) ? true : false;
+        if( $actDB )
+            DB::statement( 'SET FOREIGN_KEY_CHECKS=0;' );
+
+        Schema::dropIfExists( self::TABLENAME );
+
+        Schema::create( self::TABLENAME, function( $table )
         {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
-            $table->timestamps();
-        });
+            $table->engine = 'InnoDB';
+
+            $table->increments( 'id' )->unsigned();
+            $table->string( 'username', 32 )->unique();
+            $table->string( 'email', 255 )->unique();
+            $table->string( 'password', 160 );
+            $table->tinyInteger( 'active' )->default( 0 );
+            $table->timestamps();  // Adds created_at and updated_at columns
+            $table->softDeletes(); // Add a deleted_at column to your table // Set in Model: protected $softDelete = true;
+            //$table->smallInteger( 'attempts' )->default( 0 );
+            $table->string( 'remember_token' )->nullable();
+            $table->string( 'country_code', 2 )->default( 'de' );
+            $table->string( 'channel' )->nullable();
+            $table->string( 'product_language', 5 )->nullable();
+        } );
+
+        if( $actDB )
+            DB::statement( 'SET FOREIGN_KEY_CHECKS=1;' );
     }
+
     /**
      * Reverse the migrations.
      *
@@ -27,6 +56,16 @@ class CreateUsersTable extends Migration {
      */
     public function down()
     {
-        Schema::drop('users');
+        Eloquent::unguard();
+        $actDB = ( Config::get( 'database.default' ) === 'mysql' ) ? true : false;
+
+        if( $actDB )
+            DB::statement( 'SET FOREIGN_KEY_CHECKS=0;' );
+
+        Schema::dropIfExists( self::TABLENAME );
+
+        if( $actDB )
+            DB::statement( 'SET FOREIGN_KEY_CHECKS=1;' );
     }
+
 }
